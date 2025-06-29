cd10f92ea11da933a522ac49a064b37b3964bdde
91xxxia <2397966950@qq.com>
2025-06-29 12:01:04 +0800

    views/index

---
+++ views中的index/index.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="app-container home">
+    <el-row :gutter="20">
+      <el-col :sm="24" :lg="12" style="padding-left: 20px">
+        <h2>过敏性疾病专病库</h2>
+        <p>
+          本专病库管理系统基于rouyi框架开发，包含临床数据库、生物样本库、流调数据库、随访数据库。首次整合临床、环境、肠道菌群、基因组等数据，构建过敏性疾病
+          全生命周期数据链。采用国际通用标准，支持未来向其他疾病数据库扩展，推动数据向新药研发、健康管理产品转化。
+          多学科诊疗如何应用于疑难疾病的诊断与治疗已然成为近年来医学界讨论的热点之一，将多学科诊疗应用于过敏性疾病的诊疗，对于提升医院整体临床诊疗水平、改善患儿
+          生活质量具有重要意义。因此，在信息化、互联网、人工智能等新技术的帮助下，多学科诊疗这一新型临床诊疗模式便成为提升过敏性疾病诊疗水平的重要突破点之一。
+        </p>
+      </el-col>
+
+      <el-col :sm="24" :lg="12" style="padding-left: 50px">
+        <el-row>
+          <el-col :span="12">
+            <h2>技术选型</h2>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6">
+            <h4>后端技术</h4>
+            <ul>
+              <li>SpringBoot</li>
+              <li>Spring Security</li>
+              <li>JWT</li>
+              <li>MyBatis</li>
+              <li>Druid</li>
+              <li>Fastjson</li>
+              <li>...</li>
+            </ul>
+          </el-col>
+          <el-col :span="6">
+            <h4>前端技术</h4>
+            <ul>
+              <li>Vue</li>
+              <li>Vuex</li>
+              <li>Element-ui</li>
+              <li>Axios</li>
+              <li>Sass</li>
+              <li>Quill</li>
+              <li>...</li>
+            </ul>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-divider />
+    <el-row :gutter="20">
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>小组成员</span>
+          </div>
+          <div class="body">
+
+            <p>
+              <div><i class="el-icon-user-solid"></i> 102300425_郭育铭</div>
+              <div><i class="el-icon-user-solid"></i> 102300428_何振坤</div>
+              <div><i class="el-icon-user-solid"></i> 102300429_林哲兴</div>
+              <div><i class="el-icon-user-solid"></i> 102300430_胡喆昶</div>
+            </p>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+        <el-card class="update-log">
+          <div slot="header" class="clearfix">
+            <span>更新日志</span>
+          </div>
+          <el-collapse accordion>
+            <el-collapse-item title="v1.1.0 - 2025-06-29">
+              <ol>
+                <li>前端页面更改</li>
+                <li>其他细节优化</li>
+              </ol>
+            </el-collapse-item>
+            <el-collapse-item title="v1.0.0 - 2025-06-29">
+              <ol>
+                <li>初步完成基本框架设计</li>
+              </ol>
+            </el-collapse-item>
+          </el-collapse>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.9.0"
+    }
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank")
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
+
